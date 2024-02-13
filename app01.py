7b77491066ea6406209ca702d7218d41e8180dc8
Fenrir <159877762+zxFENRIRxz@users.noreply.github.com>
2024-02-13 20:06:29 +0700

    streamlit App

---
+++ app01.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header('My OOP NotBook')
+#streamlit run 
+import streamlit as st
+st.title('OOP in Ml')
+st.sidebar.text('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.image('https://factoryworks.aprilia.com/img/motorbikes/mm-race-1100-2022/detail-00.jpg?v018')
+st.text_input("what's your name?")
---
+++ app02.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title('OOP in Ml')
+st.sidebar.text('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.image('https://factoryworks.aprilia.com/img/motorbikes/mm-race-1100-2022/detail-00.jpg?v018')
+st.text_input("what's your name?")
